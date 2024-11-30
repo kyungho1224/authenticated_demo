43681a01ea19abffe12c14fd688fa42dd09d344d
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-11-30 23:53:33 +0900

    init project

---
+++ src/test/java/kr/amaranth/authenticated_demo/AuthenticatedDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.amaranth.authenticated_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
